57b7389c735e7210c67e71aa4c59d87a521cce73
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-01-27 07:05:07 +0700

    Edit: Class Name using kebabcase

--- src/scripts/pages/Login.js
+++ src/scripts/pages/Login.js
@@ -4,13 +4,13 @@ import { BalloonInput } from '../components';
 const Login = () => {
     return (
         <div className="login">
-            <div className="loginRight">
-                <h1 className="loginTitle">Masuk ke Schoolzy</h1>
+            <div className="login-right">
+                <h1 className="login-title">Masuk ke Schoolzy</h1>
                 <p>Selamat datang kembali! login dengan data Anda yang Anda masukkan saat pendaftaran</p>
-                <div className="loginForm">
+                <div className="login-form">
                     <BalloonInput type="email" id="email" label="Email" placeholder="example@email.com" />
                     <BalloonInput type="password" id="password" label="Password" placeholder="************" />
-                    <button className="btnSubmit">Masuk</button>        
+                    <button className="btn-submit">Masuk</button>
                 </div>
             </div>
         </div>
