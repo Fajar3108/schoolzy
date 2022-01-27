f7dd66af2a945fcb727c35003720cd21fdbde3d5
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-01-27 07:26:00 +0700

    Edit: Login Page UI

--- src/scripts/pages/Login.js
+++ src/scripts/pages/Login.js
@@ -4,13 +4,16 @@ import { BalloonInput } from '../components';
 const Login = () => {
     return (
         <div className="login">
-            <div className="login-right">
-                <h1 className="login-title">Masuk ke Schoolzy</h1>
-                <p>Selamat datang kembali! login dengan data Anda yang Anda masukkan saat pendaftaran</p>
-                <div className="login-form">
-                    <BalloonInput type="email" id="email" label="Email" placeholder="example@email.com" />
-                    <BalloonInput type="password" id="password" label="Password" placeholder="************" />
-                    <button className="btn-submit">Masuk</button>
+            <div className="container">
+                <div className="login-left"></div>
+                <div className="login-right">
+                    <h1 className="login-title">Masuk ke Schoolzy</h1>
+                    <p>Selamat datang kembali! login dengan data Anda yang Anda masukkan saat pendaftaran</p>
+                    <div className="login-form">
+                        <BalloonInput type="email" id="email" label="Email" placeholder="example@email.com" />
+                        <BalloonInput type="password" id="password" label="Password" placeholder="************" />
+                        <button className="btn-submit">Masuk</button>
+                    </div>
                 </div>
             </div>
         </div>
